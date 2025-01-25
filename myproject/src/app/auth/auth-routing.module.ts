2f9d55c94c37fa57ccc36d48c95521440ee2f854
sameer <sameerkhan9991303458@gmail.com>
2025-01-25 22:54:42 +0530

    abc

--- myproject/src/app/auth/auth-routing.module.ts
+++ myproject/src/app/auth/auth-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { SignupComponent } from './signup/signup.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import {  UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 const routes: Routes = [
   {
@@ -13,7 +13,7 @@ const routes: Routes = [
   },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  {path:'dashboard', component:UserDashboardComponent},
+  { path: 'dashboard', component: UserDashboardComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
